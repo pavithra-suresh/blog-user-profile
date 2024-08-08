d60c9099756b7ddafac3b6f88d97aed4a9d96571
pavithra-suresh <pavithrasuresh399@gmail.com>
2024-08-08 16:23:05 +0530

    Adding email constraint

--- user-profile/src/main/java/com/blog/site/user/model/UserDto.java
+++ user-profile/src/main/java/com/blog/site/user/model/UserDto.java
@@ -1,6 +1,7 @@
 package com.blog.site.user.model;
 
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ public class UserDto {
 	private String userName;
 	
 	@NotEmpty(message = "Email should not be empty")
+	@Email(message = "Email should be valid")
 	private String email;
 	
 	@NotEmpty(message = "Password should not be empty")
